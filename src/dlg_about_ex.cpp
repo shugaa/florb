ac1508aed9128dd64735c4d09c5c5aece379db03
Bjoern <bjoern@shugaa.de>
2014-07-31 22:40:32 +0200

    The about dialog now reads license information from file.

--- src/dlg_about_ex.cpp
+++ src/dlg_about_ex.cpp
@@ -1,4 +1,7 @@
 #include <string>
+#include <sstream>
+#include <iostream>
+#include <fstream>
 #include "utils.hpp"
 #include "version.hpp"
 #include "fluid/dlg_about.hpp"
@@ -9,33 +12,22 @@ void dlg_about::create_ex()
     utils::set_window_icon(m_window); 
 
     m_buf = new Fl_Text_Buffer();
-
+    
     std::string v(std::string("Version: ") + std::string(FLORB_PROGSTR));
 
     m_buf->append(v.c_str());
     m_buf->append("\n\n");
 
-    m_buf->append("Copyright (c) 2014 Bjoern Rehm <bjoern@shugaa.de>");
-    m_buf->append("\n\n");
-    m_buf->append("Distributed under the terms of the MIT license as stated below:");
-    m_buf->append("\n\n");
-    m_buf->append("Permission is hereby granted, free of charge, to any person obtaining a copy\n");
-    m_buf->append("of this software and associated documentation files (the \"Software\"), to deal\n");
-    m_buf->append("in the Software without restriction, including without limitation the rights\n");
-    m_buf->append("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n");
-    m_buf->append("copies of the Software, and to permit persons to whom the Software is\n");
-    m_buf->append("furnished to do so, subject to the following conditions:");
-    m_buf->append("\n\n");
-    m_buf->append("The above copyright notice and this permission notice shall be included in\n");
-    m_buf->append("all copies or substantial portions of the Software.");
-    m_buf->append("\n\n");
-    m_buf->append("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n");
-    m_buf->append("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY\n");
-    m_buf->append("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n");
-    m_buf->append("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n");
-    m_buf->append("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n");
-    m_buf->append("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n");
-    m_buf->append("THE SOFTWARE.");
+    std::ostringstream oss;
+    oss << RESOURCEDIR << utils::pathsep() << "/LICENSE.txt";
+    std::ifstream licfile(oss.str());
+
+    std::string line;
+    while (std::getline(licfile, line))
+    {
+        line += "\n";
+        m_buf->append(line.c_str());
+    }
 
     m_display->buffer(m_buf);
 }
