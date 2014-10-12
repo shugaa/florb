be8002bd5427a5478b1b53f56cdc908f41d793ba
Bjoern <bjoern@shugaa.de>
2014-10-12 16:38:54 +0200

    And even more release prep.

---
+++ src/mkgittags.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Check whether we have git installed
+which git > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    exit 0
+fi
+
+# Check whether this is a git repo at all
+git rev-parse --git-dir > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    exit 0
+fi
+
+git tag > gittags.txt 2>&1
