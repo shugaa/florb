2bef6dcc432d6684d6a0491fd96e31dbbf93db58
Bjoern <bjoern@shugaa.de>
2014-10-12 13:16:40 +0200

    Build system improvements for intended 1.0 release.

--- src/mkversionhpp.sh
+++ src/mkversionhpp.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
 
-FLORB_V=$( git rev-parse --short HEAD )
+# Check whether we have git installed, and do nothing if not
+which git > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    exit 0;
+fi
+
+# Check whether this is a git repo with version info at all
+git rev-parse --git-dir > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    exit 0;
+fi
+
+# shortrev version
+FLORB_V=$(  git rev-parse --short HEAD )
+
+# Use a tag on HEAD if we have one
+git describe --exact-match HEAD > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    FLORB_V=$( git describe --exact-match HEAD )
+fi
+
+# Use a commandline supplied version string (for release preparation)
+if [ $# -eq  1 ]; then
+    FLORB_V=$1
+fi
 
 echo "#ifndef VERSION_HPP" >  version.hpp
 echo "#define VERSION_HPP" >> version.hpp
---
+++ src/version.hpp
@@ -0,0 +1,6 @@
+#ifndef VERSION_HPP
+#define VERSION_HPP
+#define FLORB_VERSION     "v1.0"
+#define FLORB_PROGSTR     "florb v1.0"
+#define FLORB_USERAGENT   "florb/v1.0"
+#endif //VERSION_HPP
