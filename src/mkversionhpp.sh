5f2cf5e0a1fc5fc7a87b46230a94a00e0d86878b
Bjoern <bjoern@shugaa.de>
2014-10-12 16:21:27 +0200

    Even more fixes for release builds.

--- src/mkversionhpp.sh
+++ src/mkversionhpp.sh
@@ -1,29 +1,24 @@
 #!/bin/sh
 
+# Default version if we can't take it from git
+FLORB_V="v1.0"
+
 # Check whether we have git installed, and do nothing if not
 which git > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    exit 0;
-fi
-
-# Check whether this is a git repo with version info at all
-git rev-parse --git-dir > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    exit 0;
-fi
-
-# shortrev version
-FLORB_V=$(  git rev-parse --short HEAD )
-
-# Use a tag on HEAD if we have one
-git describe --exact-match HEAD > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    FLORB_V=$( git describe --exact-match HEAD )
-fi
+    
+    # Check whether this is a git repo with version info at all
+    git rev-parse --git-dir > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        FLORB_V=$( git rev-parse --short HEAD )
+    fi
 
-# Use a commandline supplied version string (for release preparation)
-if [ $# -eq  1 ]; then
-    FLORB_V=$1
+    # Use a tag on HEAD if we have one
+    git describe --exact-match HEAD > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        FLORB_V=$( git describe --exact-match HEAD )
+    fi
+
 fi
 
 echo "#ifndef VERSION_HPP" >  version.hpp
